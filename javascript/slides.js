97c9a02083843090c6a89acf311e73fa1d9431f2
hizz1008 <jun981008@naver.com>
2022-09-28 17:56:12 +0900

    section3 갤러리 편집

--- javascript/slides.js
+++ javascript/slides.js
@@ -41,85 +41,88 @@ function onSlide() {
 }
 
 onSlide();
-const slideProduct = document.querySelector(".slide-product")
-const slideProductSlides = document.querySelector(".slide-product_slides")
-const slideProductUl = document.querySelector(".slide-product_slides_container")
-const slideProductLi = document.querySelectorAll(".slide-product_slides_container li")
+
+const slideProduct = document.querySelector('.slide-product');
+const slideProductSlides = document.querySelector('.slide-product_slides');
+const slideProductUl = document.querySelector(
+  '.slide-product_slides_container'
+);
+const slideProductLi = document.querySelectorAll(
+  '.slide-product_slides_container li'
+);
 const slideProductCount = slideProductLi.length;
 const slideProductWidth = 620;
 const slideProductMargin = 40;
 let slideProductCurrentIdx = 0;
 
-
-function setTimeoutAnimatedClass(){
-  setTimeout(()=>{
-    slideProductUl.classList.remove("slide-product_animated")
+function setTimeoutAnimatedClass() {
+  setTimeout(() => {
+    slideProductUl.classList.remove('slide-product_animated');
     slideProductCurrentIdx = 0;
-    slideProductUl.style.left = "0%";
-  },200)
-  setTimeout(()=>{
-    slideProductUl.classList.add("slide-product_animated")
-  },258)
+    slideProductUl.style.left = '0px';
+  }, 50);
+  setTimeout(() => {
+    slideProductUl.classList.add('slide-product_animated');
+  }, 150);
 }
 
 function nextMove() {
-  slideProductCurrentIdx++
-  if (slideProductCurrentIdx <= slideProductCount-1) {
+  slideProductCurrentIdx++;
+  if (slideProductCurrentIdx <= slideProductCount - 1) {
     //슬라이드가 마지막 이미지를 벋어나지 않도록 하기 위해 작성
-    const offset = 660 * (slideProductCurrentIdx);
+    const offset = 660 * slideProductCurrentIdx;
     slideProductUl.style.left = `${-offset}px`;
     //드래그할 경우 -660만큼 앞으로 이동
   } else {
     //슬라이드가 이미지를 벗어나지 않고 다시 돌아오게 하기 위해 값을 재할당
     setTimeoutAnimatedClass();
   }
-  console.log(slideProductCurrentIdx,slideProductCount)
 }
+//드래그 슬라이드를 5s 뒤에 자동으로 넘어가게 실행
+setInterval(nextMove, 5000);
 
 function prevMove() {
   slideProductCurrentIdx--;
-  if (slideProductCurrentIdx >= -slideProductCount+4) {
+  if (slideProductCurrentIdx >= -slideProductCount + 4) {
     //슬라이드가 마지막 이미지를 벋어나지 않도록 하기 위해 작성
 
-    const offset = 660 * (slideProductCurrentIdx);
-    console.log(slideProductCurrentIdx)
+    const offset = 660 * slideProductCurrentIdx;
+
     slideProductUl.style.left = `${-offset}px`;
     //슬라이드 컨테이너의 left값이 100단위로 -가 될 수 있게 작성
   } else {
     setTimeoutAnimatedClass();
     //슬라이드가 이미지를 벗어나지 않고 다시 돌아오게 하기 위해 값을 재할당
   }
-  console.log(slideProductCurrentIdx,slideProductCount)
 }
 
-slideProduct.addEventListener("mousedown",(e)=>{
+slideProduct.addEventListener('mousedown', (e) => {
   pressed = true;
   startX = e.offsetX - slideProductUl.offsetLeft;
-})
+  slideProductUl.classList.remove('slide-product_animated');
+});
 
-window.addEventListener("mouseup",()=>{
+window.addEventListener('mouseup', () => {
   pressed = false;
-})
+  slideProductUl.classList.add('slide-product_animated');
+});
 
-slideProduct.addEventListener("mousemove",(e)=>{
-  
-  if(!pressed) return;
-  e.preventDefault()
-  x = e.offsetX
-  slideProductUl.style.left = `${x-startX}px`;
-})
-
-
+slideProduct.addEventListener('mousemove', (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+  x = e.offsetX;
+  slideProductUl.style.left = `${x - startX}px`;
+});
 
 let startPoint = 0;
 let endPoint = 0;
 
 // PC 클릭 이벤트 (드래그)
-slideProduct.addEventListener("mousedown", (e) => {
+slideProduct.addEventListener('mousedown', (e) => {
   startPoint = e.pageX; // 마우스 드래그 시작 위치 저장
 });
 
-slideProduct.addEventListener("mouseup", (e) => {
+slideProduct.addEventListener('mouseup', (e) => {
   endPoint = e.pageX; // 마우스 드래그 끝 위치 저장
   if (startPoint < endPoint) {
     // 마우스가 오른쪽으로 드래그 된 경우
@@ -132,36 +135,50 @@ slideProduct.addEventListener("mouseup", (e) => {
 
 makeClone();
 
-function makeClone(){
-  for(let i =0; i<slideProductCount; i++){
-    let cloneSlide = slideProductLi[i].cloneNode(true)
-    cloneSlide.classList.add("clone")
+function makeClone() {
+  // 슬라이드 이미지 클론 복사
+  for (let i = 0; i < slideProductCount; i++) {
+    let cloneSlide = slideProductLi[i].cloneNode(true);
+    // 슬라이드 갯수만큼 클론노드 사용
+    cloneSlide.classList.add('clone');
+    // 슬라이드에 클론 클래스 추가
     slideProductUl.appendChild(cloneSlide);
+    // 기존 이미지 뒤에 생성
   }
-  for(let i = slideProductCount-1; i>=0; i--){
-    let cloneSlide = slideProductLi[i].cloneNode(true)
-    cloneSlide.classList.add("clone")
+  for (let i = slideProductCount - 1; i >= 0; i--) {
+    let cloneSlide = slideProductLi[i].cloneNode(true);
+    cloneSlide.classList.add('clone');
     slideProductUl.prepend(cloneSlide);
+    // 기존 이미지 앞에 생성
   }
   updateWidth();
   setInitialPos();
-  setTimeout(()=>{
-    slideProductUl.classList.add("slide-product_animated")
-  },100);
-
+  setTimeout(() => {
+    slideProductUl.classList.add('slide-product_animated');
+  }, 100);
 }
-function updateWidth(){
-  slideProductUl.style.width = (slideProductWidth + slideProductMargin)*slideProductCount - slideProductMargin + "px";
-  const slideProductLi = document.querySelectorAll(".slide-product_slides_container li")
+function updateWidth() {
+  // 클론 이미지가 추가된 슬라이드의 넓이를 구하는 함수
+  slideProductUl.style.width =
+    (slideProductWidth + slideProductMargin) * slideProductCount -
+    slideProductMargin +
+    'px';
+  const slideProductLi = document.querySelectorAll(
+    '.slide-product_slides_container li'
+  );
   let newslideCount = slideProductLi.length;
 
-  let newWidth = (slideProductWidth+slideProductMargin)*newslideCount - slideProductMargin + "px";
+  let newWidth =
+    (slideProductWidth + slideProductMargin) * newslideCount -
+    slideProductMargin +
+    'px';
 
   slideProductUl.style.width = newWidth;
 }
-function setInitialPos(){
-  const initialTranslateValue = -(slideProductWidth+slideProductMargin)*slideProductLi.length
+function setInitialPos() {
+  const initialTranslateValue =
+    -(slideProductWidth + slideProductMargin) * slideProductLi.length;
 
-  slideProductUl.style.transform = 'translateX('+initialTranslateValue+'px)';
+  slideProductUl.style.transform =
+    'translateX(' + initialTranslateValue + 'px)';
 }
-
---
+++ javascript/yScroll.js
@@ -0,0 +1,16 @@
+// const introduceExhibition = document.querySelector('.introduce_exhibition');
+// const introduceContent = document.querySelector(
+//   '.introduce_exhibition_content'
+// );
+// let introduceHight = introduceContent.clientHeight;
+// let introTop = introduceContent.scrollTop;
+// window.addEventListener('scroll', function () {
+//   let introScrollY = window.scrollY;
+//   introduceContent.style.top = `${-introScrollY}px`;
+// });
+// introduceContent.onfocus = (e) => {
+//   console.log('히히');
+// };
+/*introduceContent가 마우스 포커싱이 되면 함수가 실행
+introduceContent의 높이 top값을 스크롤 값의 -px로 설정
+*/
